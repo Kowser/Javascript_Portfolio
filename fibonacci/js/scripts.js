54fd82913abb4e177d564ff54aa5b2be6d8e205b
Michal <student@epicodus.com>
2013-11-13 17:36:28 -0800

    add fibonacci without recursion

--- fibonacci/js/scripts.js
+++ fibonacci/js/scripts.js
@@ -1,12 +1,27 @@
 var sequence = [0,1];
+function isInteger(index) {
+  if ($.isNumeric(index) && index > 0 && index % 1 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 function fibonacci(index) {
 	index -= 1;
-
   if (index >= 2) {
-    sequence.push(sequence[sequence.length - 2] + sequence[sequence.length - 1]);
+    sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
     fibonacci(index);
   }
   return sequence[index];
+}
+
+// fibonacci w/o recursion
+function fibonacciNR(index) {
+  for (var i = index - 2; i > 0; i--) {
+    sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
+  }
+  return sequence[index - 1];
 }
 
 $(function() {
@@ -14,10 +29,10 @@ $(function() {
     var index = $("input#index").val();
      $("div#result").hide().fadeIn(500);
 
-    if (!$.isNumeric(index) || index <= 0 || index % 1 > 0) {   
-      $("div#result").empty().append("That is <strong>not</strong> a valid index (<strong>Integers only</strong>)!");
+    if (isInteger(index)) {   
+      $("div#result").empty().append("The fibonacci number at index <strong>" + index + "</strong> is <strong>" + fibonacci(index) + "</strong>.");      
     } else {
-      $("div#result").empty().append("The fibonacci number at index <strong>" + index + "</strong> is <strong>" + fibonacci(index) + "</strong>.");
+      $("div#result").empty().append("That is <strong>not</strong> a valid index (<strong>Integers only</strong>)!");
     }
 
     $("input#index").val('');
--- fibonacci/spec/specs.js
+++ fibonacci/spec/specs.js
@@ -1,10 +1,31 @@
 describe('FIBONACCI', function() {
+  it("has a starting array with 2 numbers", function() {
+    sequence.length.should.equal(2);
+  }); 
+
+  describe('isInteger', function() {
+    it("Checks that input is an integer", function() {
+      isInteger("dog").should.equal(false);
+    });
+
+    it("Checks that given number is an integer", function() {
+      isInteger(7.1).should.equal(false);
+    });
+
+    it("Checks that given number is an integer", function() {
+      isInteger(7.0).should.equal(true);
+    });
+  });
+
 	describe("fibonacci", function() {
 		it("returns the fibonacci number at a given index", function() {
 			fibonacci(5).should.equal(3);
 		});
 	});
-  it("has a starting array with 2 numbers", function() {
-    sequence.length.should.equal(2);
-  }); 
+
+  describe("fibonacciNR", function() {
+    it("returns the fibonacci number at a given index", function() {
+      fibonacciNR(6).should.equal(5);
+    });
+  });
 });
